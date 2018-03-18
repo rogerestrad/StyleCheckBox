b6b6d367ddc828c50224e0607b58f2393d5e408e
Edgar Rogelio <rogerestrad@gmail.com>
2018-03-18 13:25:06 -0600

    Initial commit

---
+++ app/src/main/java/com/rogerestrad/stylecheckbox/MainActivity.java
@@ -0,0 +1,42 @@
+package com.rogerestrad.stylecheckbox;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    CheckBox chkBold,chkItalic;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.txt);
+        chkBold = (CheckBox) findViewById(R.id.chkBold);
+        chkItalic = (CheckBox) findViewById(R.id.chkItalic);
+        chkBold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (chkBold.isChecked())
+                    editText.setTypeface(null, Typeface.BOLD);
+                else
+                    editText.setTypeface(null, Typeface.NORMAL);
+            }
+        });
+        chkBold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (chkBold.isChecked())
+                    editText.setTypeface(null, Typeface.ITALIC);
+                else
+                    editText.setTypeface(null, Typeface.NORMAL);
+            }
+                
+
+        });
+    }
+
+}
